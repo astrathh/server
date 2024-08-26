c784e1e96db7ca8df1a172aa8d6e5034d514280d
astrathh <astratnho@hotmail.com>
2024-08-26 15:57:40 -0300

    error fix no index,js

--- src/index.js
+++ src/index.js
@@ -7,21 +7,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 const questRoutes = require('./routes/questRoutes');
 
-app.use(cors());
+// Configurando o CORS para aceitar requisições apenas de um domínio específico
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(helmet());
 
-// Configurando o CORS para aceitar requisições apenas de um domínio específico
-const corsOptions = {
-    origin: 'http://localhost:3000/',
-    optionsSuccessStatus: 200
-  };
-  
-  app.use(cors(corsOptions));
 // Usando as rotas
 app.use('/api', questRoutes);
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
