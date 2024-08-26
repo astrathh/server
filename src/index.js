d3b7e950b9bafa70b19e3fe39ba7546cad8e8c4f
astrathh <astratnho@hotmail.com>
2024-08-26 16:48:07 -0300

    refactor: Update CORS configuration in index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 // server/src/index.js
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 5000;
 const questRoutes = require('./routes/questRoutes');
 
-// Configurando o CORS para aceitar requisições apenas de um domínio específico
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
-app.use(morgan('combined'));
+app.use(cors({
+  origin: 'http://localhost:3000', // URL do seu frontend
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
-app.use(helmet());
 
 // Usando as rotas
 app.use('/api', questRoutes);
