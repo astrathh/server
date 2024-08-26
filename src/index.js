9b62d06b785403e89c6c552e6a3ba40610ed2cc0
astrathh <astratnho@hotmail.com>
2024-08-26 11:13:51 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ const port = process.env.PORT || 5000;
 const questRoutes = require('./routes/questRoutes');
 
 app.use(cors());
+app.use(morgan('combined'));
 app.use(express.json());
+app.use(helmet());
 
+// Configurando o CORS para aceitar requisições apenas de um domínio específico
+const corsOptions = {
+    origin: 'http://localhost:3000/',
+    optionsSuccessStatus: 200
+  };
+  
+  app.use(cors(corsOptions));
 // Usando as rotas
 app.use('/api', questRoutes);
 
