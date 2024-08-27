b524a71fd13aaa024539804c6d51eb19883c940e
astrathh <astratnho@hotmail.com>
2024-08-27 12:33:27 -0300

    refactor: Update CORS configuration in index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 const questRoutes = require('./routes/questRoutes');
 
+// Configurando o CORS
+const allowedOrigins = process.env.NODE_ENV === 'production' 
+  ? ['https://seu-frontend.vercel.app'] 
+  : ['http://192.168.15.11:3000'];
+
+  console.log('Allowed Origins:', allowedOrigins);
 app.use(cors({
-  origin: 'http://localhost:3000', // URL do seu frontend
+  origin: allowedOrigins,
   methods: ['GET', 'POST'],
   credentials: true
 }));
+
 app.use(express.json());
 
 // Usando as rotas
 app.use('/api', questRoutes);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://192.168.15.11:3000`);
 });
