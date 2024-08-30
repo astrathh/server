369b87c878395b7ccb5b814c0f0bdcecafe4fbf8
astrathh <astratnho@hotmail.com>
2024-08-30 09:57:12 -0300

    cors update v5

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@ const port = process.env.PORT || 5000;
 const questRoutes = require('./routes/questRoutes');
 
 // Configurando o CORS
-const corsOptions = {
+
+
+const allowedOrigins = process.env.NODE_ENV === 'production' 
+? ['https://diagnostico.growthagency.com.br/'] 
+: ['http://192.168.15.11:3000'];
+
+console.log('Allowed Origins:', allowedOrigins);
+
+app.use(cors({
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   preflightContinue: false,
   optionsSuccessStatus: 204
-};
+}));
 
-app.use(cors(corsOptions));
 
 app.use(express.json());
 
