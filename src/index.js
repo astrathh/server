80f84b05d099635de7150917366416e8decb3b76
astrathh <astratnho@hotmail.com>
2024-08-30 09:30:16 -0300

    refactor: Update CORS configuration in index.js

--- src/index.js
+++ src/index.js
@@ -7,19 +7,18 @@ const questRoutes = require('./routes/questRoutes');
 
 // Configurando o CORS
 const corsOptions = {
-  origin: '*',
+  origin: 'https://diagnostico.growthagency.com.br/',  // Domínio específico
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  preflightContinue: false,
+  credentials: true,  // Mantém o envio de cookies
   optionsSuccessStatus: 204
 };
+
 app.use(cors(corsOptions));
+
+app.options('https://diagnostico.growthagency.com.br/', cors(corsOptions));
 
 app.use(express.json());
 
 // Usando as rotas
 app.use('/api', questRoutes);
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://192.168.15.11:3000`);
-});
